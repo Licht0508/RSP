36729b42fd7868e6a170cdb03a3ab3bcf4a5d74e
中村理陽人 <r.nakamura2508@gmail.com>
2023-05-24 15:48:30 +0900

    add files

---
+++ RSP/Input.cs
@@ -0,0 +1,43 @@
+using System;
+
+class Input
+{   
+    public enum Hand
+    {
+        ROCK,
+        SCISSORS,
+        PAPER
+    }
+    public Hand hand = Hand.ROCK;
+
+    /// <summary>
+    /// 出す手を入力するメソッド
+    /// </summary>
+    public void InputHand()
+    {   
+        bool canFinish = false;
+        while(!canFinish)
+        {
+            Console.Write("Please enter Rock, Scissors or Paper : ");
+            string str = Console.ReadLine();
+            // 入力された手に応じて変数に格納する値を変更する
+            switch(str)
+            {
+                case "Rock":
+                    hand = Hand.ROCK;
+                    canFinish = true;
+                    break;
+                case "Scissors":
+                    hand = Hand.SCISSORS;
+                    canFinish = true;
+                    break;
+                case "Paper":
+                    hand = Hand.PAPER;
+                    canFinish = true;
+                    break;
+                default:
+                    canFinish = false;
+            }
+        }
+    }
+}
--- RSP/RSP.cs
+++ RSP/RSP.cs
@@ -1,7 +1,7 @@
 using System;
 
 class RSP
-{
+{   
     static void Main()
     {
         
