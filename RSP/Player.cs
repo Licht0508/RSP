c3287c822ffa9f8f609d0d1ca4315065d2cca9dc
koutano <koutano0311@gmail.com>
2023-05-31 13:55:18 +0900

    add Player.cs

---
+++ RSP/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSP
+{
+    internal class Player
+    {
+
+        public Input input;
+
+        /// <summary>
+        /// Playerコンストラクタ
+        /// </summary>
+        Player()
+        {
+            input = new Input();
+        }
+        
+        /// <summary>
+        /// 出す手を決めるメソッド
+        /// </summary>
+        public void InputHand()
+        {
+            input.InputHand();
+        }
+
+        /// <summary>
+        /// Playerの現在の手確認用メソッド
+        /// </summary>
+        /// <returns>現在の手</returns>
+        public Hand GetHand() { return input.hand; }
+
+    }
+}
