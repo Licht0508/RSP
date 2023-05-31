693dabc5a3700a123ca369a3a74018f52677eeaf
koutano <koutano0311@gmail.com>
2023-05-31 14:13:31 +0900

    fix Player.cs

--- RSP/Player.cs
+++ RSP/Player.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RSP
+﻿namespace RSP
 {
     internal class Player
     {
@@ -24,7 +18,7 @@ namespace RSP
         /// </summary>
         public void InputHand()
         {
-            input.InputHand();
+            input.DetermineToPutHand();
         }
 
         /// <summary>
